dce52f0eda9d8539e292e3000bbd6bddce185fe9
Ali Shahabzadeh <129598985+AliShahabzadeh@users.noreply.github.com>
2025-03-14 15:25:32 +0100

    Update ASMulator.java
    add: "lab" instruction
    add: "jmp" instruction

--- src/ASMulator.java
+++ src/ASMulator.java
@@ -12,6 +12,9 @@ public class ASMulator {
 
         String[] varNames = new String[0];
         int[] varValues = new int[0];
+        
+        String[] labNames = new String[0];
+        int[] labLines = new int[0];
 
         BufferedReader br = null;
 
@@ -21,12 +24,12 @@ public class ASMulator {
 
             int numLines = Integer.parseInt(br.readLine());
 
-            for(int i = 0; i < numLines; i++){
+            for(int i = 0; i < 10; i++){
 
                 String instruction = br.readLine();
-                String[] instructionParts = instruction.split(" ");
+                String[] instructionParts = instruction.split("\\s+");
 
-                //add
+                // add <var> <num>
                 if(instructionParts[0].equals("add") && instructionParts.length == 3){
 
                     boolean find = false;
@@ -60,7 +63,7 @@ public class ASMulator {
 
                 }
 
-                //sub
+                // sub <var> <num>
                 else if(instructionParts[0].equals("sub") && instructionParts.length == 3){
 
                     for(int j = 0; j < varValues.length; j++){
@@ -75,7 +78,7 @@ public class ASMulator {
 
                 }
 
-                //mul
+                // mul <var> <num>
                 else if(instructionParts[0].equals("mul") && instructionParts.length == 3){
 
                     for(int j = 0; j < varValues.length; j++){
@@ -89,6 +92,40 @@ public class ASMulator {
                     }
 
                 }
+                
+                // lab <name>
+                else if(instructionParts[0].equals("lab") && instructionParts.length == 2){
+                    
+                    labNames = Arrays.copyOf(labNames, labNames.length + 1);
+                    labNames[labNames.length - 1] = instructionParts[1];
+
+                    labLines = Arrays.copyOf(labLines, labLines.length + 1);
+                    labLines[labLines.length - 1] = i;
+                    
+                }
+                
+                // jmp <var> <num> <name>
+                else if(instructionParts[0].equals("jmp") && instructionParts.length == 4){
+                    
+                    for(int j = 0; j < varNames.length; j++){
+                        
+                        if(varNames[j].equals(instructionParts[1]) && varValues[j] == Integer.parseInt(instructionParts[2])){
+                            
+                            for(int k = 0; k < labNames.length; k++){
+                                
+                                if(labNames[k].equals(instructionParts[3])){
+                                    
+                                    i = labLines[k];
+                                    
+                                }
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
             }
 
         } catch (FileNotFoundException e) {
@@ -96,6 +133,13 @@ public class ASMulator {
         } catch (IOException e) {
             System.out.println(e.toString());
         }
+        
+        int temp = 0;
+        
+        for(int i = 0; i < varValues.length; i++)
+            temp+=varValues[i];
+        
+        System.out.println(temp + "");
 
     }
 
